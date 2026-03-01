6f80a870688702e02fa8e151362edc16eb295dff
Oliwia Kleybor <kleyboro@gmail.com>
2026-03-01 10:26:11 +0100

    addListSiema

--- testapp/lib/pages/lol_page.dart
+++ testapp/lib/pages/lol_page.dart
@@ -10,7 +10,6 @@ class LolPage extends StatefulWidget {
 class _LolPageState extends State<LolPage> {
   final myController = TextEditingController();
   final List<String> listSiema = [];
-
   @override
   void dispose() {
     myController.dispose();
@@ -20,46 +19,41 @@ class _LolPageState extends State<LolPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Moja Lista')),
+      appBar: AppBar(),
       body: Column(
         children: [
-          const Center(child: Text('hello')),
-          const SizedBox(height: 10),
+          Center(child: Text('hello')),
           SizedBox(
             height: 55.0,
             width: 290,
             child: TextField(
               controller: myController,
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'siema',
               ),
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: listSiema.length, // Tutaj też małe 'l'
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(listSiema[index]), // I tutaj
-                  leading: const Icon(Icons.label),
-                );
-              },
-            ),
+          Expanded(child: ListView.builder(
+            itemCount: listSiema.length,
+            itemBuilder: (context, index){
+              return ListTile(
+                title: Text(listSiema[index]),
+                leading: const Icon(Icons.label),
+              );
+            })),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 600.0),
+            child: ElevatedButton(onPressed: () {
+              if (myController.text.isNotEmpty){
+                setState(() {
+                  listSiema.add(myController.text);
+                  myController.clear();
+                });
+              }
+            }, child: Text('add')),
           ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (myController.text.isNotEmpty) {
-            setState(() {
-              // Teraz nazwa zgadza się z definicją na górze
-              listSiema.add(myController.text);
-              myController.clear();
-            });
-          }
-        },
-        child: const Icon(Icons.add),
       ),
     );
   }
